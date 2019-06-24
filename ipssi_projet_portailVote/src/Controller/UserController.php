540b696b80a093f96f6e276b609642d775dc6ac3
Aissatou Diop <aissatoudiop@Aissatous-MacBook-Pro.local>
2019-06-24 08:45:08 -0700

    formulaire inscription

--- ipssi_projet_portailVote/src/Controller/UserController.php
+++ ipssi_projet_portailVote/src/Controller/UserController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class UserController extends AbstractController
 {
@@ -16,4 +21,23 @@ class UserController extends AbstractController
             'controller_name' => 'UserController',
         ]);
     }
+
+    public function inscription( Request $request) : Response{
+
+        $newUserForm = $this->createForm(UserType::class);
+        $newUserForm->handleRequest($request);
+        if($newUserForm->isSubmitted() && $newUserForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newUserForm->getData());
+            $em->flush();
+            //$isOk = true;
+        }
+        return $this->render('user/userInscription.html.twig', [
+            'newUserForm' => $newUserForm->createView(),
+            //'isOk' => $isOk,
+        ]);
+    }
+
+
+
 }
